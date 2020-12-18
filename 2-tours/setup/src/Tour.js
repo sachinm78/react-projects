c4d3160441ca808ef103cd08081b5ab36a44f7a4
sachinm78 <sachinm78@gmail.com>
2020-12-17 18:00:54 -0600

    created removeTours function and conditional rendering of tour info; project 2 complete

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, name, price, removeTour }) => {
-  
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -10,10 +10,13 @@ const Tour = ({ id, image, info, name, price, removeTour }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-          <p>
-            {info}
-          </p>
-        <button className="delete-btn">
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'show less' : '  read more'}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
           not interested
         </button>
       </footer>
