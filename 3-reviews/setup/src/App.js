f773d25b725968b2716b7aeabf4552099c28223d
sachinm78 <sachinm78@gmail.com>
2020-12-18 16:37:15 -0600

    set up app.js to display page title and Review component: set up state in Review component

--- 3-reviews/setup/src/App.js
+++ 3-reviews/setup/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Review from './Review';
 // import { FaGithubSquare } from 'react-icons/fa'
+// inside the return statement, you can invoke the icon
+{/* <FaGithubSquare className="icon"/> */}
+
 function App() {
   return (
-    <h2>
-      reviews project setup
-      {/* <FaGithubSquare className="icon"/> */}
-    </h2>
+    <main>
+      <section className='container'>
+        <h2>our reviews</h2>
+        <div className='underline'>
+
+        </div>
+        <Review />
+      </section>
+    </main>
   )
 }
 
