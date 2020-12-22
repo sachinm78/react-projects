f6b4ae008f1fdab1b121fc78f3dff3158efa175c
sachinm78 <sachinm78@gmail.com>
2020-12-22 17:26:54 -0600

    set up Categories component; created filterItems function in App.js

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -2,10 +2,20 @@ import React, { useState } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
-  const [menuItems, setMenuItems] = useState(items)
-  const [categories, setCategories] = useState()
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
@@ -14,11 +24,11 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
-  )
+  );
 }
 
 export default App;
