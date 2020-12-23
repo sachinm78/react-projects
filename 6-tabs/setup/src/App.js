fe62f5ad755deaa98d90064f954720fb67a73e0a
sachinm78 <sachinm78@gmail.com>
2020-12-23 15:22:43 -0600

    ran npm install; set up state management in App.js

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -4,6 +4,10 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
+  
   return <h2>tabs project setup</h2>
 }
 
