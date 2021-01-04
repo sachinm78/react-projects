cc50e8e632300ecd931a2654b0b2a4808fe5777a
sachinm78 <sachinm78@gmail.com>
2021-01-04 15:25:07 -0600

    set up useEffect to recycle through data on button press

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -5,6 +5,17 @@ import data from './data';
 function App() {
   const [people, setPeople] =useState(data)
   const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
   return (
     <section className="section">
       <div className="title">
