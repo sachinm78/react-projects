2dc98df85f873ebbdaf9cb72c3cf9b6e36540602
sachinm78 <sachinm78@gmail.com>
2021-01-05 16:19:36 -0600

    set up handleSubmit and error handling

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -8,17 +8,30 @@ function App() {
   const [error, setError] = useState(false)
   const [list, setList] = useState([])
 
-  const handleSubmit =(e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('hello')
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
   }
+
   return (
     <>
-      <section className="container">
+      <section className='container'>
         <h3>color generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type="text" value={color} onChange={(e)=> setColor(e.target.value)} placeholder="#f15025"/>
-          <button className="btn" type="submit">
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>
             submit
           </button>
         </form>
