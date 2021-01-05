ca841e532b7e83d962c7449731572bbaef979416
sachinm78 <sachinm78@gmail.com>
2021-01-05 16:10:38 -0600

    added submit button and placeholder text

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -17,7 +17,10 @@ function App() {
       <section className="container">
         <h3>color generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type="text" value={color}/>
+          <input type="text" value={color} onChange={(e)=> setColor(e.target.value)} placeholder="#f15025"/>
+          <button className="btn" type="submit">
+            submit
+          </button>
         </form>
       </section>
       <section className="colors">
