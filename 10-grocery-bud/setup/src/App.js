429f835c74d3d20291c7ac0865cf8012e7759f75
sachinm78 <sachinm78@gmail.com>
2021-01-06 13:37:00 -0600

    setting up alert functionality

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault()
     if (!name) {
-      // display alert
+      showAlert(true, 'danger', 'please enter value');
     } else if (name && isEditing) {
         // deal with edit
     } else {
@@ -22,10 +22,14 @@ function App() {
     }
   };
 
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg });
+  };
+
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} />}
         <h3>grocery bud</h3>
         <div className='form-control'>
         <input
