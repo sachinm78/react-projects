edb74e524db5e549265f301310e601cf47a477c6
sachinm78 <sachinm78@gmail.com>
2021-01-06 13:06:36 -0600

    set up form and handle submit

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -8,9 +8,29 @@ function App() {
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('hello')
+  };
 
   return (
     <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+        <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
         <div className='grocery-container'>
           <List />
           <button className='clear-btn'>
