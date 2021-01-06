04ddd6a3b7996815b52f767dbb4ade76cea7556d
sachinm78 <sachinm78@gmail.com>
2021-01-06 13:45:54 -0600

    set up clearList function

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -15,7 +15,7 @@ function App() {
     } else if (name && isEditing) {
         // deal with edit
     } else {
-        // show alert
+        showAlert(true, 'success', 'item added to the list');
         const newItem = { id: new Date().getTime().toString(), title: name };
         setList([...list, newItem]);
         setName('');
@@ -24,6 +24,11 @@ function App() {
 
   const showAlert = (show = false, type = '', msg = '') => {
     setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, 'danger', 'empty list');
+    setList([]);
   };
 
   return (
@@ -47,7 +52,7 @@ function App() {
       {list.length > 0 && (
         <div className='grocery-container'>
           <List items={list} />
-          <button className='clear-btn'>
+          <button className='clear-btn' onClick={clearList}>
             clear items
           </button>
         </div>
