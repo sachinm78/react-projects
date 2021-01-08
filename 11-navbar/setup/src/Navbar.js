acaf56cb8c2340a44d19bc52e4de07ad42d1cd4a
sachinm78 <sachinm78@gmail.com>
2021-01-08 14:38:20 -0600

    finished scaffolding in Navbar.js

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -16,11 +16,36 @@ const Navbar = () => {
         <div className="links-container show-container">
           <ul className='links'>
             <li>
-              <a href="#"></a>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">contact</a>
+            </li>
+            <li>
+              <a href="#">products</a>
             </li>
           </ul>
         </div>
-        <ul className="social-icons"></ul>
+        <ul className="social-icons">
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   )
