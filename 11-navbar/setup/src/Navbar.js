625ac494485530e3ece23a60a063a49351535239
sachinm78 <sachinm78@gmail.com>
2021-01-08 14:28:31 -0600

    successfully rendering logo and nav toggle

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -8,9 +8,18 @@ const Navbar = () => {
     <nav>
       <div className="nav-center">
         <div className="nav-header">
-
+          <img src={logo} alt='logo' />
+          <button className='nav-toggle'>
+            <FaBars />
+          </button>
         </div>
-        <div className="links-container show-container"></div>
+        <div className="links-container show-container">
+          <ul className='links'>
+            <li>
+              <a href="#"></a>
+            </li>
+          </ul>
+        </div>
         <ul className="social-icons"></ul>
       </div>
     </nav>
