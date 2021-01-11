c5181ac1bcb2e06637439417f64f0b62c58b9ed1
sachinm78 <sachinm78@gmail.com>
2021-01-11 16:02:48 -0600

    submenu complete

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,34 +1,48 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+const AppContext = React.createContext();
 
-const AppContext = React.createContext()
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [page, setPage] = useState({ page: '', links: [] });
+  const [location, setLocation] = useState({});
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
 
-export const AppProvider = ({children}) => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
-    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false)
-
-    const openSidebar = () => {
-        setIsSidebarOpen(true)
-    }
-    const closeSidebar = () => {
-        setIsSidebarOpen(false)
-    }
-    const openSubmenu = () => {
-        setIsSubmenuOpen(true)
-    }
-    const closeSubmenu = () => {
-        setIsSubmenuOpen(false)
-    }
-
-
-
-    return <AppContext.Provider value={{
-        isSubmenuOpen, isSidebarOpen, openSubmenu, openSidebar, closeSubmenu, closeSidebar
-    }}>
-        {children}
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
+      }}
+    >
+      {children}
     </AppContext.Provider>
-}
-
+  );
+};
+// make sure use
 export const useGlobalContext = () => {
-    return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
